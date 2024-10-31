1322dc8f51024c1ea22d2527ce66339adc4e7513
lauroteles <laurotfl@gmail.com>
2024-10-31 17:38:27 -0300

    primeiro commit

---
+++ controller.py
@@ -0,0 +1,300 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+import sqlalchemy as sa
+import streamlit as st
+from sqlalchemy.exc import DBAPIError
+import time
+import functools
+from psycopg2 import pool
+from sqlalchemy import create_engine
+import threading
+
+
+import psycopg2
+from psycopg2 import sql
+
+
+class dbController():
+    
+    def __init__(self,banco):
+        
+        username = st.secrets["apiAWS"]["username"]
+        password = st.secrets["apiAWS"]["password"]
+        host = st.secrets["apiAWS"]["host"]
+        port = st.secrets["apiAWS"]["port"]
+        self.engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/jcp', 
+                        pool_size=2, max_overflow=1, pool_recycle=5, pool_timeout=10, pool_pre_ping=True, pool_use_lifo=True)                                
+
+        self.conn = self.engine.connect()
+
+
+
+    def closeCons(self):
+ 
+        self.engine.dispose()
+        self.conn.close()
+
+    def registrar_contato_clientes(self,df,tabela):
+        df.to_sql(tabela, self.engine, if_exists='append', index=False)
+
+
+    def inserirTabelas(self, tabela, df):
+
+        try:
+            verificacaoCNPJ = df.iloc[0]['CNPJ']
+            verificacaoAno = df.iloc[0]['Ano']
+            query = text(f"""SELECT * 
+                        FROM {tabela} 
+                        WHERE \"CNPJ\" = :CNPJ AND \"Ano\" = :ano""")
+            
+            with self.engine.connect() as conn:
+                result = conn.execute(query, {'CNPJ': verificacaoCNPJ, 'Ano': verificacaoAno})
+                rows = result.fetchall()
+        except:
+            pass
+                    
+        try:    
+            verificacaoCNPJ = df.iloc[0]['CNPJ']
+            verificacaoAnoTanalise = df.iloc[0]['PeriodoDeAnalise']
+            
+            query = text(f"""SELECT * 
+                            FROM {tabela} 
+                            WHERE \"CNPJ\" = :CNPJ AND \"PeriodoDeAnalise\" = :PeriodoDeAnalise""")
+
+            with self.engine.connect() as conn:
+                result = conn.execute(query, {'CNPJ': verificacaoCNPJ, 'PeriodoDeAnalise': verificacaoAnoTanalise})
+                rows = result.fetchall()
+        except:
+            pass
+            
+        try:
+            verificacaoCNPJ = df.iloc[0]['CNPJ']
+            
+            query = text(f"""SELECT * 
+                            FROM {tabela} 
+                            WHERE \"CNPJ\" = :CNPJ """)
+
+            with self.engine.connect() as conn:
+                result = conn.execute(query, {'CNPJ': verificacaoCNPJ})
+                rows = result.fetchall()
+        except Exception as e:
+            st.warning(e)
+            pass
+            
+        if rows:
+            st.warning(f"CNPJ {verificacaoCNPJ}  já existe na tabela {tabela}. Não inserindo dados.")
+        else:
+            df.to_sql(tabela, self.engine, if_exists='append', index=False)  
+                
+
+        self.closeCons()
+
+
+    def get_data_by_cnpj(self, cnpj,tabela):
+        query = f"SELECT * FROM {tabela} WHERE \"CNPJ\" = '{cnpj}'"
+        with self.engine.connect():
+            
+            df = pd.read_sql_query(query, self.engine)  
+        self.closeCons()
+        return df
+
+    @functools.lru_cache
+    def get_jcp_value(self, cnpj: str, tabela: str, ano: int, operation: str) -> pd.DataFrame:
+        query = f"""
+            SELECT "CNPJ", "Ano", "Value","Operation"
+            FROM {tabela}
+            WHERE "CNPJ" = '{cnpj}' AND "Ano" = '{ano}' AND "Operation" = '{operation}'
+        """
+        with self.engine.connect():
+            
+            df = pd.read_sql_query(query, self.engine)
+            self.closeCons()
+        return df
+
+    @functools.lru_cache
+    def get_jcp_value_trimestral(self, cnpj: str, tabela: str, ano: int, operation: str) -> pd.DataFrame:
+        query = f"""
+            SELECT "CNPJ", "Ano", "Value 1º Trimestre","Value 2º Trimestre","Value 3º Trimestre","Value 4º Trimestre",
+            "Operation 1º Trimestre",
+            "Operation 2º Trimestre",
+            "Operation 3º Trimestre",
+            "Operation 4º Trimestre"
+            FROM {tabela}
+            WHERE "CNPJ" = '{cnpj}' AND "Ano" = '{ano}' 
+            AND "Operation 1º Trimestre" = '{operation}' 
+            AND "Operation 2º Trimestre" = '{operation}' 
+            AND "Operation 3º Trimestre" = '{operation}' 
+            AND "Operation 4º Trimestre" = '{operation}'  """
+        
+        with self.engine.connect():
+            
+            df = pd.read_sql_query(query, self.engine) 
+
+        self.closeCons()
+        return df
+    
+    def deletarDadosDaTabela(self,tabela):
+        query = text("DELETE FROM {}".format(tabela))
+        
+        self.conn.execute(query)
+        print(f'Os valores para tabela {tabela} foram DELETADOS!')
+        self.conn.commit()
+   
+    def deletarDadosDaTabelaPorCnpj(self, cnpj, tabela):
+        # Use parameterized query to avoid SQL injection and properly handle data types
+        self.engine
+        self.conn = self.engine.connect()
+        query = text(f"DELETE FROM {tabela} WHERE \"CNPJ\" = :cnpj")
+        self.conn.execute(query, {"cnpj": cnpj})
+        print(f'Os valores para tabela {tabela} e CNPJ {cnpj} foram DELETADOS!')
+        self.conn.commit()
+        self.closeCons()
+
+
+    def get_all_data(self,tabela):
+        query = f"SELECT * FROM {tabela}"
+        
+        with self.engine.connect():
+                df = pd.read_sql_query(query, self.engine)
+        self.closeCons()
+        return df
+
+    @functools.lru_cache
+    def queryResultadoFinal(self, cnpj,tabela,ano):
+        
+        query = f""" 
+        SELECT "CNPJ", "Ano", "Value","Operation","index"
+        FROM {tabela}
+        WHERE \"CNPJ\" = '{cnpj}' AND \"Ano\" = '{ano}'"""
+        query2 = f""" 
+        SELECT "CNPJ", "Ano", "Value","Operation"
+        FROM {tabela}
+        WHERE \"CNPJ\" = '{cnpj}' AND \"Ano\" = '{ano}'"""
+        with self.engine.connect():
+            
+            try:
+                df = pd.read_sql_query(query, self.engine)
+            except:
+                df = pd.read_sql_query(query2, self.engine)
+
+        self.closeCons()        
+        return df
+
+    @functools.lru_cache
+    def queryResultadoFinalTrimestral(self, cnpj,tabela,ano):
+        query = f"""
+        SELECT "CNPJ", "Ano","Operation 1º Trimestre" ,"Value 1º Trimestre","Operation 2º Trimestre" ,"Value 2º Trimestre",
+        "Operation 3º Trimestre" ,"Value 3º Trimestre","Operation 4º Trimestre" ,"Value 4º Trimestre","index"
+            FROM {tabela}
+            WHERE "CNPJ" = '{cnpj}' AND "Ano" = '{ano}' """
+        
+        query2 = f"""
+        SELECT "CNPJ", "Ano","Operation 1º Trimestre" ,"Value 1º Trimestre","Operation 2º Trimestre" ,"Value 2º Trimestre",
+        "Operation 3º Trimestre" ,"Value 3º Trimestre","Operation 4º Trimestre" ,"Value 4º Trimestre"
+            FROM {tabela}
+            WHERE "CNPJ" = '{cnpj}' AND "Ano" = '{ano}' """
+
+        with self.engine.connect(): 
+        
+            try:
+                df = pd.read_sql_query(query, self.engine)
+            except:
+                df = pd.read_sql_query(query2, self.engine)
+        self.closeCons()
+        return df
+    
+    def inserirTabelasFinaisJCP(self, tabela, df):
+
+        self.engine
+        self.conn = self.engine.connect()
+        verificacaoAno = int(df.iloc[0]['Ano'])
+        verificacaoCNPJ = df.iloc[0]['CNPJ']
+
+        query = text(f"SELECT * FROM {tabela} WHERE \"Ano\" = :Ano AND \"CNPJ\" = :CNPJ")
+        
+        result = self.conn.execute(query, {'Ano': verificacaoAno, 'CNPJ': float(verificacaoCNPJ)})
+
+        rows = result.fetchall()
+
+        if rows:
+            st.warning(f"Ano {verificacaoAno} e CNPJ {verificacaoCNPJ} já existem na tabela {tabela}. Não inserindo dados.")
+        else:
+            with self.engine.connect():
+                
+                df.to_sql(tabela, self.engine, if_exists='append', index=False)
+            
+        self.closeCons()
+
+    def update_table(self, tabela, df, cnpj, ano):
+        operations = df['Operation'].unique()
+        self.engine
+        self.conn = self.engine.connect()
+        
+        # Gerenciador de contexto para a transação
+        with self.conn.begin() as transaction:
+           
+                for operation in operations:
+                    value = float(df.loc[df['Operation'] == operation, 'Value'].iloc[0])
+                    query = text(f"UPDATE {tabela} SET \"Value\" = :Value WHERE \"CNPJ\" = :CNPJ AND \"Ano\" = :Ano AND \"Operation\" = :Operation")
+                    params = {'Value': value, 'CNPJ': cnpj, 'Ano': ano, 'Operation': operation}
+                    self.conn.execute(query, params)
+                
+        self.closeCons()
+
+    def update_table_trimestral(self, tabela, df, cnpj, ano):
+        self.engine
+        self.conn = self.engine.connect()
+        operations = [op for trimestre in [1,2,3,4] for op in df[f'Operation {trimestre}º Trimestre'].unique()]
+    # Gerenciador de contexto para a transação
+        with self.conn.begin() as transaction:
+            
+                for trimestre in [1,2,3,4]:
+                    for operation in operations:
+                        filtered_df = df.loc[df[f'Operation {trimestre}º Trimestre'] == operation]
+                        if not filtered_df.empty:
+                            value = float(filtered_df[f'Value {trimestre}º Trimestre'].iloc[0])
+                            query = text(f"UPDATE {tabela} SET \"Value {trimestre}º Trimestre\" = :Value WHERE \"CNPJ\" = :CNPJ AND \"Ano\" = :Ano AND \"Operation {trimestre}º Trimestre\" = :Operation")
+                            params = {'Value': value, 'CNPJ': cnpj, 'Ano': ano, 'Operation': operation}
+                            self.conn.execute(query, params)
+                        else:
+                            print(f'Os valores para {trimestre}º trimestre foram atualizados')
+
+        self.closeCons()
+
+
+
+
+if __name__ =="__main__":
+
+    
+    controler = dbController('taxall')
+    
+
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','l100')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','l300')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','m300')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','m350')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','n630')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','n670')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','resultadosjcp')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','resultadosjcptrimestral')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','tipodaanalise')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','cadastrodasempresas')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','lacslalur')
+    controler.deletarDadosDaTabelaPorCnpj('09302262000185','lacslalurtrimestral')
+
+
+    # controler.deletarDadosDaTabela('l100')
+    # controler.deletarDadosDaTabela('l300')
+    # controler.deletarDadosDaTabela('m300')
+    # controler.deletarDadosDaTabela('m350')
+    # controler.deletarDadosDaTabela('n630')
+    # controler.deletarDadosDaTabela('n670')
+    # controler.deletarDadosDaTabela('resultadosjcp')
+    # controler.deletarDadosDaTabela('resultadosjcptrimestral')
+    # controler.deletarDadosDaTabela('tipodaanalise')
+    # controler.deletarDadosDaTabela('cadastrodasempresas')
+    # controler.deletarDadosDaTabela('lacslalur')
+    # controler.deletarDadosDaTabela('lacslalurtrimestral')
+
+
---
+++ registroPedidosCliestes.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import streamlit as st
+import psycopg2
+import base64
+from bitrix24 import Bitrix24
+import re
+from requests import get
+
+from controller import dbController
+
+controler = dbController('ECF')
+st.set_page_config(layout='wide')
+background_image ="Untitleddesign.jpg"
+st.markdown(
+     f"""
+     <iframe src="data:image/jpg;base64,{base64.b64encode(open(background_image, 'rb').read()).decode(
+
+    )}" style="width:4000px;height:3000px;position: absolute;top:-3vh;right:-1250px;opacity: 0.5;background-size: cover;background-position: center;"></iframe>
+     """,
+     unsafe_allow_html=True )
+from bitrix24 import Bitrix24
+import re
+from requests import get
+
+url = 'postgresql+psycopg2://postgres:djgr27041965@localhost:5432/db_compensacao'
+
+bx24 = Bitrix24('https://alldax.bitrix24.com.br/rest/13210/zwvynbqn2q6vfw8m/')
+
+try:
+    data = bx24.callMethod('crm.company.list', select=['ID', 'TITLE', 'UF_CRM_1634750723', 'UF_CRM_63580C7CDFB5C', 'UF_CRM_630FC29A707BC', 'UF_CRM_1691072587'], filter={'UF_CRM_1727790100': '360938'})
+    df = pd.DataFrame(data)
+    df['UF_CRM_63580C7CDFB5C'] = df['UF_CRM_63580C7CDFB5C'].str.replace('1379','MEI - Micro Empreendedor').str.replace('1381','Simples Nacional').str.replace('1383','Lucro Presumido').str.replace('1385','Lucro Real').str.replace('13225','Inapta').str.replace('174146','N/A').fillna('---')
+    df.rename(columns={'TITLE': 'nome_cliente', 'UF_CRM_1634750723': 'cod_cnpj', 'UF_CRM_63580C7CDFB5C': 'regime_tributario', 'UF_CRM_630FC29A707BC':'grupo_empresarial', 'UF_CRM_1691072587':'atividades_economicas'}, inplace=True)
+    df= df.dropna()
+    df['nome_cliente'] = df['nome_cliente'].astype(str).str.upper().apply(lambda x: re.sub(r'^\d+\s-\s', '', x) if isinstance(x, str) and re.match(r'^\d+\s-\s', x) else x).str.replace('Ç', 'C').str.replace('Ã', 'A').str.replace('Á', 'A').str.replace('Â', 'A').str.replace('É','E').str.replace('Ô','O').str.replace('Õ','O').str.replace('Í','I')
+    df['grupo_empresarial'] = df['grupo_empresarial'].str.replace('0', 'Não').str.replace('1', 'Sim')
+    df['atividades_economicas'] = df['atividades_economicas'].str. strip()
+    df = df.drop(columns=['ID'])
+    df = df.sort_values(by = 'nome_cliente')
+    df['cod_cnpj'] = df['cod_cnpj'].str.replace('.','').str.replace('/','').str.replace('-','')
+    
+
+except Exception as e:
+    st.error(f"Ocorreu um erro ao chamar a API do Bitrix24: {e}")
+
+
+
+
+tabelaDeNomes = df
+listaDosNomesDasEmpresas = list(tabelaDeNomes['nome_cliente'])
+nome_para_cnpj = dict(zip(tabelaDeNomes['nome_cliente'], tabelaDeNomes['cod_cnpj']))
+
+dataframesParaDownload = []
+tabelaParaRelatorio = []
+nomeEmpresaSelecionada = st.sidebar.selectbox('Selecione a empresa para registro de informações',listaDosNomesDasEmpresas)
+cnpj_selecionado = nome_para_cnpj.get(nomeEmpresaSelecionada, "")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    
+    data = st.date_input('Data')
+    data_formatada = data.strftime('%d/%m/%Y')
+    horario = st.time_input('Horario do contao')
+    motivo = st.selectbox('Motivo :',options=['Cobrança', 'Reclamação','Duvidas'],placeholder='')
+    responsalve = st.text_input('Responsavel pelo contato')
+    observacoes = st.text_area('Observações ')
+
+    tabela = {'Data':[data],
+            'Horario':[horario],
+            'Motivo':[motivo],
+            'Responsavel':[responsalve],
+            'Observacoes':[observacoes],
+            'cnpj':[cnpj_selecionado]
+            }
+    tabela_df = pd.DataFrame(tabela)
+    
+    registrar = st.button('Registrar informações')
+    if registrar:
+        try:
+            controler.registrar_contato_clientes(tabela_df,'contato_clientes')
+            st.warning('Dados salvos')
+        except Exception as e:
+            st.warning(e)
+
+
+with col2:
+    with st.form('tabela_para_visualização'):
+        filtro_empresa = st.selectbox('Filtro Empresa',listaDosNomesDasEmpresas)
+
+        if st.form_submit_button('Filtrar'):
+            dados_totais_da_tabela =controler.get_all_data('contato_clientes')
+            dados_totais_da_tabela['cnpj'] = dados_totais_da_tabela['cnpj'].astype(str).str[:-2]
+            cnpj_selecionado_filtro = nome_para_cnpj.get(filtro_empresa, "")
+            dados_totais_da_tabela = dados_totais_da_tabela[dados_totais_da_tabela['cnpj']==cnpj_selecionado_filtro]
+        
+        else:
+            
+            dados_totais_da_tabela =controler.get_all_data('contato_clientes')
+        
+        st.dataframe(dados_totais_da_tabela)
+
+
+
